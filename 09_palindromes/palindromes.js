dcf6b3293da79a4d4e05a88ac65bb2831abd2bb9
alutton <andrew.lutton@gmail.com>
2022-03-15 18:47:55 -0400

    Add solution to ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, "");
+    reverseStr = str.split("").reverse().join("");
+    return str === reverseStr;
 };
 
 // Do not edit below this line
