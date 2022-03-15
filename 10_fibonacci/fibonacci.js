14ca76620ee2e806b3961307c6a0eb8bdd60c1ec
alutton <andrew.lutton@gmail.com>
2022-03-15 19:08:10 -0400

    Add solution to ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (seq) {
+    let fibArr = [1, 1];
+    let i = 1;
+    seqNum = +seq
+    if (seqNum > 0) {
+        while (i < seqNum - 1) {
+            i++;
+            fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+        }
+        return fibArr[seqNum - 1];
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
