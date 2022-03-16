57807135fa5f4273c1cbc8a80aa88a6e6959b20d
alutton <andrew.lutton@gmail.com>
2022-03-16 09:51:22 -0400

    Add solution to ex13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,21 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+    // const re = /^[A-Za-z]/g;
+    const strArr = str.split("")
 
-};
+    if (shift < 0) { shift = (shift % 26) + 26 };
+
+    const shiftMap = strArr.map((char) => {
+        let charCode = char.charCodeAt(0);
+        if (charCode >= 65 && charCode <= 90) {
+            charCode = (charCode - 65 + shift) % 26 + 65;
+        } else if (charCode >= 97 && charCode <= 122) {
+            charCode = (charCode - 97 + shift) % 26 + 97;
+        }
+        return String.fromCharCode(charCode);
+    });
+    return shiftMap.join("")
+}
+    ;
 
 // Do not edit below this line
 module.exports = caesar;
