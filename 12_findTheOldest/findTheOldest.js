1f1603cc8d0be683858bb5ee68f1c8b60a8b4173
alutton <andrew.lutton@gmail.com>
2022-03-16 08:18:12 -0400

    Add solution to ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleToday = people.forEach((person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = (new Date().getFullYear());
+        };
+    });
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
